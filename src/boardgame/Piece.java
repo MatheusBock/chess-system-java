a5e6d37b37aadf1a90ed76df857d04f8f8e6a1f0
Matheus Bock <bockmatheus@gmail.com>
2021-01-18 22:46:52 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 
 	protected Position position;
 	private Board board;
@@ -16,5 +16,24 @@ public class Piece {
 
 	protected Board getBoard() {
 		return board;
+	}
+	
+	public abstract boolean[][] possibleMoves();
+	
+	//Hook method: Método que faz um gancho com a subclasse. Dessa forma, esse método só será chamado pela outra classe que
+	//herdar essa classe abstrata.
+	//Há um padrão de projeto chamado Template Method, em que um método concreto utiliza um método abstrato.
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	public boolean isThereAnyPossibleMoves() {
+		for (int i = 0; i < possibleMoves().length; i++) {
+			for (int j = 0; j < possibleMoves().length; j++) {
+				if (possibleMoves()[i][j]) 
+					return true;
+			}
+		}
+		return false;
 	}
 }
